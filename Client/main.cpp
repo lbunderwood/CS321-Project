c1a3bd842fe6c766cd4e225dcffcc30141e73175
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2021-03-12 15:10:20 -0900

    Created basic client-side program

---
+++ Client/main.cpp
@@ -0,0 +1,24 @@
+// Created 3/12/2021 by Luke Underwood
+// CS321-Project
+// main.cpp
+// main function for client portion of DM project
+
+#include <iostream>
+#include <string>
+#include "../Lib/Connection.h"
+
+int main(int argc, char** argv)
+{
+    std::string address;
+    int port = 0;
+    if (argc == 3)
+    {
+        address = argv[1];
+        port = std::stoi(argv[2]);
+    }
+
+    Connection server(address, port);
+    server.connect();
+    server.sendInfo("Hello, world!");
+}
+
