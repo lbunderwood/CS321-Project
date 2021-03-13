36bea8fe05895a962c40a3dfe8bfb14d79138817
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2021-03-12 15:23:30 -0900

    Created very basic server side program

---
+++ Server/main.cpp
@@ -0,0 +1,23 @@
+// Created 3/12/2021 by Luke Underwood
+// CS321-Project
+// main.cpp
+// main function for client portion of DM project
+
+#include <iostream>
+#include <string>
+#include "../Lib/Connection.h"
+
+int main()
+{
+    int port = 58426;
+    Connection clients("", port);
+
+    while (true)
+    {
+        clients.acceptIncoming();
+        for (auto n : clients.receiveInfo())
+        {
+            std::cout << n;
+        }
+    }
+}
